6cbdd6d99283fad5ca6aea0cf4d5485895dfa75f
Juanchoa31 <juandavidzapata53@gmail.com>
2025-09-03 20:57:08 -0500

    updated comments in the body project

--- demo/demo/src/main/java/com/tdea/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/tdea/demo/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class DemoApplication {
 
     public static void main(String[] args) {
+    	//Activa la aplicación spring boot
         SpringApplication.run(DemoApplication.class, args);
     }
 }
